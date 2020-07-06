01a3894038b23600681d6b6428286f08c4ed5eea
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-06 17:51:23 +0530

    initial setup

---
+++ session1/examples/initial_setup.sh
@@ -0,0 +1,26 @@
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d
+$ cd d:/U-Bank
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank
+$ git init
+#Initialized empty Git repository in D:/U-Bank/.git/
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$ git remote add origin https://github.com/ishwar-soni/U-Bank.git
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$ git config --global user.name "Ishwar Soni"
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$ git config --global user.email ishwar.soni@upgrad.com
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$ git config user.name
+#Ishwar Soni
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$ git config user.email
+#ishwar.soni@upgrad.com
+
+#Ishwar.Soni@UE-B-LAP-0121 MINGW64 /d/U-Bank (master)
+$
