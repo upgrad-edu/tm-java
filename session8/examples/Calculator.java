d44266f5aef1fe4e526a165150cd35a382524260
Ishwar Soni <ishwar.soni@upgrad.com>
2020-07-29 14:41:37 +0530

    calculator

---
+++ session8/examples/Calculator.java
@@ -0,0 +1,65 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+interface Operation {
+    int operate(int x, int y);
+}
+
+class Addition implements Operation {
+    @Override
+    public int operate(int x, int y) {
+        return x + y;
+    }
+}
+
+class Subtraction implements Operation {
+    @Override
+    public int operate(int x, int y) {
+        return x - y;
+    }
+}
+
+public class Calculator {
+
+    private Map<String, Operation> supportedOperations;
+
+    private void load () {
+        supportedOperations = new LinkedHashMap<>();
+        Operation addition = new Addition();
+        Operation subtraction = new Subtraction();
+        supportedOperations.put("add", addition);
+        supportedOperations.put("sub", subtraction);
+    }
+
+    public void start () {
+        load();
+        Scanner scanner = new Scanner(System.in);
+        String selectedOperation;
+        System.out.print("Currently supported operations: ");
+        for (String opr: supportedOperations.keySet()) {
+            System.out.print("'" + opr + "'" + ", ");
+        }
+        System.out.println("Type 'EXIT' to close");
+
+        do {
+            System.out.print("Type here: ");
+            selectedOperation = scanner.nextLine();
+            if (!supportedOperations.containsKey(selectedOperation)) {
+                continue;
+            }
+            System.out.print("Enter first argument: ");
+            int x = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Enter second argument: ");
+            int y = Integer.parseInt(scanner.nextLine());
+
+            System.out.println(supportedOperations.get(selectedOperation).operate(x, y));
+            System.out.println("**********************************");
+        } while (!selectedOperation.equals("EXIT"));
+    }
+
+    public static void main(String[] args) {
+        new Calculator().start();
+    }
+}
