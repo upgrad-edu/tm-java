fbaaeff97ccd5233650ab179d7e22417d4e100e3
Ishwar Soni <ishwar.soni@upgrad.com>
2020-08-02 12:45:43 +0530

    Timer using Runnable

--- session9/examples/Timer.java
+++ session9/examples/Timer.java
@@ -1,30 +1,32 @@
-public class Timer extends Thread {
+public class Timer implements Runnable {
 
     private int interval;
+    private String name;
 
     public Timer (String name, int interval) {
-        super(name);
+        this.name = name;
         this.interval = interval;
     }
 
     @Override
     public void run() {
-        System.out.println(this.getName() + ": starting");
+        System.out.println(this.name + ": starting");
         while (interval-- > 0) {
-            System.out.println(this.getName() + ": tick");
+            System.out.println(this.name + ": tick");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println(this.getName() + ": time's up");
+        System.out.println(this.name + ": time's up");
     }
 
     public static void main(String[] args) {
-        Thread timer1 = new Timer("timer 1", 5);
+        Thread timer1 = new Thread(new Timer("timer 1", 5));
         timer1.start();
-        Thread timer2 = new Timer("timer 2", 8);
+
+        Thread timer2 = new Thread(new Timer("timer 2", 8));
         timer2.start();
     }
 }
